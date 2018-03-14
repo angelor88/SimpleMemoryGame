1f5985b81de882d3f4f2340f7b901e58ae0c544e
Alexander Neumann and Angelo Russoniello <alexander@>
2018-03-14 09:22:28 -0700

    Change images. Add GenerateCards to main.js

---
+++ js/Card.js
@@ -0,0 +1,11 @@
+export function Card(id, imgUrl){
+  this.id = id;
+  this.imgUrl = imgUrl;
+}
+// Card.prototype.CompareTo = function(inputCard){
+//   if(this.id == InputCard.id){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import '../css/styles.css';
+import { Card } from './../js/Card.js';
+
+var uniqueCards = [];
+// var displayCards = [];
+
+function GenerateCards(){
+  for(var i = 1; i <= 36; i ++){
+    let myImgPath = "../img/" + i + ".png";
+    let newCard = new Card(i,myImgPath);
+    uniqueCards.push(newCard);
+  }
+  console.log(uniqueCards);
+}
+$(document).ready(function(){
+  GenerateCards();
+});
